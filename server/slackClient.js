75da88024c11274d6f1666951179856553bf3e9d
parkerb84 <fenratsu@gmail.com>
2018-10-23 13:49:38 -0400

    Hepa is now able to respond to messages.

--- server/slackClient.js
+++ server/slackClient.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const { RTMClient } = require('@slack/client');
+let rtm = null;
 
 function handleOnAuthenticated(rtmStartData) {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+}
+
+function handleOnMessage(message) {
+  console.log(message);
+  rtm.sendMessage('*Beep Boop*', message.channel, function messageSent() {
+    
+  });
 }
 
 function addAuthenticatedHandler(rtm, handler) {
@@ -12,8 +20,9 @@ function addAuthenticatedHandler(rtm, handler) {
 
 module.exports.init = function slackClient(token, logLevel) {
   // The client is initialized and then started to get an active connection to the platform
-  const rtm = new RTMClient(token, {logLevel: logLevel});
+  rtm = new RTMClient(token, {logLevel: logLevel});
   addAuthenticatedHandler(rtm, handleOnAuthenticated);
+  rtm.on('message', handleOnMessage);
   return rtm;
 }
 
