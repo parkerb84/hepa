34eaf54cafb84eb8155584b7e439f362d96080dd
parkerb84 <fenratsu@gmail.com>
2018-10-30 14:53:44 -0400

    Added the service registry class to handle registering services and added this functionality to the service code

--- server/service.js
+++ server/service.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const service = express();
+const ServiceRegistry = require('./serviceRegistry');
+const serviceRegistry = new ServiceRegistry();
 
 service.put('/service/:intent/:port', (req, res, next) => {
   const serviceIntent = req.params.intent;
@@ -10,6 +12,7 @@ service.put('/service/:intent/:port', (req, res, next) => {
   const serviceIp = req.connection.remoteAddress.includes('::')
   ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
 
+  serviceRegistry.add(serviceIntent, serviceIp, servicePort);
   res.json({result: `${serviceIntent} at ${serviceIp}:${servicePort}`});
 })
 
