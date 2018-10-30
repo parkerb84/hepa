261bbfabced340c7362fd0845fdcf0031f1e05aa
parkerb84 <fenratsu@gmail.com>
2018-10-30 13:49:45 -0400

    Connected the Hepa-Time Microservice to the main Hepa App

--- server/intents/timeIntent.js
+++ server/intents/timeIntent.js
@@ -1,9 +1,23 @@
 'use strict';
 
+const request = require('superagent');
+
 module.exports.process = function process(intentData, cb) {
   if (intentData.intent[0].value !== 'time') {
     return cb(new Error(`Expected time intent, got ${intentData.intent[0].value}`));
   }
   if(!intentData.location) return cb(new Error('Missing location in time intent'));
-  return cb(false, `I don't yet know the time in ${intentData.location[0].value}`);
+  
+  const location = intentData.location[0].value;
+
+  request.get(`http://localhost:3010/service/${location}`, (err, res) => {
+    if(err || res.statusCode != 200 || !res.body.result) {
+      console.log(err);
+      console.log(res.body);
+
+      return cb(false, `I had a problem finding out the time in ${location}`);
+    }
+
+    return cb(false, `In ${location} it is now ${res.body.result}`);
+  })
 }
