f44e3f62c40e983199d686d24d1e365328095e3d
parkerb84 <fenratsu@gmail.com>
2018-10-30 14:27:11 -0400

    Added put service request for handling intent requests

--- server/service.js
+++ server/service.js
@@ -3,4 +3,14 @@
 const express = require('express');
 const service = express();
 
+service.put('/service/:intent/:port', (req, res, next) => {
+  const serviceIntent = req.params.intent;
+  const servicePort = req.params.port;
+  
+  const serviceIp = req.connection.remoteAddress.includes('::')
+  ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+
+  res.json({result: `${serviceIntent} at ${serviceIp}:${servicePort}`});
+})
+
 module.exports = service;
